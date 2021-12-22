010a5cee72864cad9dc1ad7e13356e14aa16446d
Bruna Gattei Micheletti <brunagattei@hotmail.com>
2021-12-22 13:43:22 -0300

    criação das rotas das paginas do aplicativo

--- src/moduloControlerLogin.js
+++ src/moduloControlerLogin.js
@@ -8,19 +8,30 @@ class ControlerUserLogin {
     checkLogin() {
         this.status = true;
     }
+    checkoutLogin(){
+        this.status = false;
+    }
     loginInfo(user, senha) {
+        
         var infoCheck = userLogin.find((userLogin) => userLogin.username === user);
 
         if (user === null || senha === null || user === "" || senha === "") {
+            this.checkoutLogin();
+            localStorage.status=this.status;
             return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "camposVazios1"));
         } else {
             if (!infoCheck) {
+                this.checkoutLogin();
+                localStorage.status=this.status;
                 return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "usuarioNaoExiste1"));
             } else {
                 if (infoCheck.senha === senha) {
                     this.checkLogin();
+                    localStorage.status=this.status;
                     return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "loginExecutado1"));
                 } else {
+                    this.checkoutLogin();
+                    localStorage.status=this.status;
                     return(comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1"));
                 }
             }
